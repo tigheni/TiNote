3c7c8a83d5f35c216b92586cd5d8a1ef384b83db
tighei <oussama.adame12@gmail.com>
2024-03-13 21:01:52 +0100

    Add Babel loader for JavaScript files in with webpack

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,26 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: "asset/resource",
             },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: {
+                    and: [/node_modules/], // Exclude libraries in node_modules ...
+                    not: [
+                        // Except for a few of them that needs to be transpiled because they use modern syntax
+                        /unfetch/,
+                        /d3-array|d3-scale/,
+                        /@hapi[\\/]joi-date/,
+                    ],
+                },
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", { targets: "defaults" }],
+                        ],
+                    },
+                },
+            },
         ],
     },
     plugins: [
